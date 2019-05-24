b711cd8c0694ad4adbad0bc732b9d4506cbd1968
Daniel Montero <d.montero95@gmail.com>
2019-05-23 23:44:30 -0600

    Installed multer and working on Add Function

--- app/controllers/admin.js
+++ app/controllers/admin.js
@@ -6,3 +6,16 @@ exports.getFunctions = (req, res, next) => {
         functions: [{ name: 'Calculadora', id: 'FN0100-000001' }]
     });
 };
+
+exports.postAddFunction = (req, res, next) => {
+    //Add User function to db
+    //Look for dependencies
+
+    // Render Account Page
+    res.render('functions/my-functions', {
+        pageTitle: 'Puggy Wrap API - My Functions',
+        path: '/my_functions',
+        isAuthenticated: true,
+        functions: [{ name: 'Calculadora', id: 'FN0100-000001' }]
+    });
+};
