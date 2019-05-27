f488ac241a02d3bc7a29bc641461da43e6778ca9
Daniel Montero <d.montero95@gmail.com>
2019-05-27 00:36:14 -0600

    Working on AddFunction
    Functions added to db, adding userId still pending.
    Started add-function view

--- app/controllers/admin.js
+++ app/controllers/admin.js
@@ -1,35 +1,63 @@
 const ApiFunction = require('../models/function');
+const Tag = require('../models/tag');
 
-exports.getFunctions = (req, res, next) => {
+
+/**
+ * FUNCTIONS
+ */
+exports.getFunctions = async(req, res, next) => {
+    const functions = await ApiFunction.findAll();
+    if (!functions) {
+        throw new Error('Error getting all functions!');
+    }
+
     res.render('functions/functions', {
         pageTitle: 'Puggy Wrap API - Functions',
         path: '/functions',
         isAuthenticated: true,
-        functions: [{ name: 'Calculadora', id: 'FN0100-000001' }]
+        functions: functions
     });
 };
 
-exports.postAddFunction = (req, res, next) => {
-    const { name, description, function_code, tags, dependencies } = req.body;
+/**
+ * ADD FUNCTION
+ */
+
+exports.getAddFunction = (req, res, next) => {
+    res.render('functions/add-function', {
+        pageTitle: 'Puggy Wrap API - Add Function',
+        path: '/add_function',
+        isAuthenticated: true,
+        errorMessage: ''
+    });
+}
+
+exports.postAddFunction = async(req, res, next) => {
+    const { name, description, function_code, dependencies } = req.body;
+    let tags = req.body.tags;
     tags = tags.replace(/\s/g, '').split(',');
     //Add User function to db
-    ApiFunction.create({
-            name,
-            description,
-            function_code
-        })
-        .then(result => {
-            // Add tags to db table
-            //Look for dependencies
+    const newFunction = await ApiFunction.create({
+        name: name,
+        description: description,
+        function_code: function_code
+    });
+    if (!newFunction) {
+        throw new Error('An error occured while creating function!');
+    }
+    // Add tags to db table
+    tags.forEach(async tag => {
+        newTag = await Tag.create({ name: tag });
+        if (!newTag) {
+            throw new Error('An error occured while creating tags')
+        }
+    });
+    //Look for dependencies
+    if (dependencies) { // are there dependencies?
+        // Add dependencies to intermediate table
+    }
 
-            // Render Account Page
-            res.render('functions/my-functions', {
-                pageTitle: 'Puggy Wrap API - My Functions',
-                path: '/my_functions',
-                isAuthenticated: true,
-                functions: [{ name: 'Calculadora', id: 'FN0100-000001' }]
-            });
-        })
-        .catch(err => console.log('An error occured while creating function:', err));
+    // Render Account Page
+    res.redirect('/admin/functions');
 
 };
