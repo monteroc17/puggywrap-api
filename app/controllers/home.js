f53c10629ba9aabeac48bf27c324f3fa983f4a45
Daniel Montero <d.montero95@gmail.com>
2019-06-02 15:44:21 -0600

    Get function code by url done!

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const beautify = require('js-beautify');
 const ApiFunction = require('../models/function');
 const Dependencies = require('../models/dependency');
 
@@ -31,23 +32,25 @@ exports.getFunctionCode = async(req, res, next) => { //returns the function code
     const functionID = req.params.functionID;
     const func = await ApiFunction.findByPk(functionID);
     if (!func) {
-        throw new Error('Function does not exist!');
+        throw new Error(`Function does not exist! id: ${functionID}`);
     }
     const FILE_PATH = `${global.__basedir}/app/util/dynamic-js/${func.name}.js`;
     let stream = fs.createWriteStream(FILE_PATH, { flags: 'a' });
-    stream.write(func.function_code);
     //include dependencies
     getDependencies(functionID)
         .then(data => {
             data.forEach(element => {
+                stream.write(`function ${element.name.replace(/\s/g,'')}(){`)
                 stream.write(element.function_code);
+                stream.write('}\n');
             });
+            stream.write(func.function_code);
             stream.end();
             fs.readFile(FILE_PATH, { encoding: 'utf-8' }, (err, data) => {
                 console.log('reading...');
                 if (!err) {
                     res.type('.js');
-                    res.send(data);
+                    res.send(beautify(data));
                     fs.unlink(FILE_PATH, err => {
                         if (err) throw new Error(err);
                         console.log('file deleted!');
