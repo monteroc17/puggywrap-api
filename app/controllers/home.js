7dc81fbb5addf615e91d7d0bf6a6af21f9ecfdcc
Daniel Montero <d.montero95@gmail.com>
2019-06-02 14:34:17 -0600

    Get Code with dependencies

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const ApiFunction = require('../models/function');
+const Dependencies = require('../models/dependency');
 
 exports.getGetStarted = (req, res, next) => {
     res.render('getting_started/getting_started', {
@@ -15,12 +16,16 @@ exports.getFunctionFile = async(req, res, next) => { //returns the function as a
     if (!func) {
         throw new Error('Function does not exist!');
     }
-    let stream = fs.createWriteStream(`${global.__basedir}/app/util/dynamic-js/${func.name}.js`, { flags: 'a' });
+    const FILE_PATH = `${global.__basedir}/app/util/dynamic-js/${func.name}.js`;
+    let stream = fs.createWriteStream(FILE_PATH, { flags: 'a' });
     stream.write(func.function_code);
     //include dependencies
     stream.end();
-    const file = `${global.__basedir}/app/util/dynamic-js/${func.name}.js`;
-    res.download(file);
+    res.download(FILE_PATH);
+    fs.unlink(FILE_PATH, err => {
+        if (err) throw new Error(err);
+        console.log('file deleted!');
+    });
 };
 exports.getFunctionCode = async(req, res, next) => { //returns the function code
     const functionID = req.params.functionID;
@@ -32,16 +37,43 @@ exports.getFunctionCode = async(req, res, next) => { //returns the function code
     let stream = fs.createWriteStream(FILE_PATH, { flags: 'a' });
     stream.write(func.function_code);
     //include dependencies
-    stream.end();
-    fs.readFile(FILE_PATH, { encoding: 'utf-8' }, (err, data) => {
-        console.log('reading...');
-        if (!err) {
-            res.type('.js');
-            res.send(data);
-            fs.unlink(FILE_PATH, err => {
-                if (err) throw new Error(err);
-                console.log('file deleted!');
+    getDependencies(functionID)
+        .then(data => {
+            data.forEach(element => {
+                stream.write(element.function_code);
             });
-        }
-    });
+            stream.end();
+            fs.readFile(FILE_PATH, { encoding: 'utf-8' }, (err, data) => {
+                console.log('reading...');
+                if (!err) {
+                    res.type('.js');
+                    res.send(data);
+                    fs.unlink(FILE_PATH, err => {
+                        if (err) throw new Error(err);
+                        console.log('file deleted!');
+                    });
+                }
+            });
+        })
+        .catch(err => console.log(err));
 };
+
+
+const getDependencies = async(id) => {
+    let promesas = [];
+    const dependencies = await Dependencies.findAll({ where: { parent_id: id } });
+    dependencies.forEach((d) => {
+        promesas.push(ApiFunction.findByPk(d.dependency_id));
+        // codes.push(dependency.function_code)
+        //stream.write(dependency.function_code);
+    });
+    return new Promise((resolve, reject) => {
+        let codes = [];
+        Promise.all(promesas)
+            .then(responses => {
+                responses.forEach(r => { codes.push(r) });
+                resolve(codes);
+            })
+            .catch(err => console.log(err));
+    });
+}
