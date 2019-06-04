a7aa3e38e603693d435882d9d61d66d7ba1c64b9
Daniel Montero <d.montero95@gmail.com>
2019-06-03 22:49:47 -0600

    User stored in DB!

--- app/routes/login.js
+++ app/routes/login.js
@@ -7,5 +7,6 @@ const isAuth = require('../middleware/is-auth');
 const router = express.Router();
 
 router.get('/login', authController.getLogin);
+router.post('/login', authController.postLogin);
 
 module.exports = router;
