78357ec75b3b926cd0687ec70aeaf1ce687d4155
Daniel Montero <d.montero95@gmail.com>
2019-06-04 16:40:07 -0600

    Route protection

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -3,6 +3,7 @@ const ApiFunction = require('../models/function');
 const Dependencies = require('../models/dependency');
 
 exports.getGetStarted = (req, res, next) => {
+    console.log(req.session.isLoggedIn);
     res.render('getting_started/getting_started', {
         pageTitle: 'Puggy Wrap API - Getting Started',
         path: '/getting_started',
