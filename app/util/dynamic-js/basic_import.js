6c2bf40b0dcd0939fdea16a462ac62c334f38c88
Daniel Montero <d.montero95@gmail.com>
2019-06-09 23:34:27 -0600

    change to address in basic import file

--- app/util/dynamic-js/basic_import.js
+++ app/util/dynamic-js/basic_import.js
@@ -1,7 +1,7 @@
 function Importar(...funciones) {
     funciones.forEach(funcion => {
         const script = document.createElement('script');
-        script.src = `http://localhost:8000/fn-${funcion[0]}?version=${funcion[1]}`;
+        script.src = `http://68.183.100.14:8000/fn-${funcion[0]}?version=${funcion[1]}`;
         document.body.appendChild(script);
     });
 }
